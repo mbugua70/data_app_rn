00f0fdc3601c0e748efca1638fda2eb8db63f7e2
Code MBU <johnmbugua23code@gmail.com>
2025-01-27 12:33:41 +0300

    adding dashboard screen to the project

--- screens/Login.js
+++ screens/Login.js
@@ -1,86 +1,68 @@
-import { useEffect, useState, useContext } from 'react';
-import { View, Text, StyleSheet} from 'react-native'
-
+import { useEffect, useState, useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useMutation } from "@tanstack/react-query";
+import { LoginHander, queryClient } from "../http/api";
 
 // import LoadingOverlay from '../components/ui/LoadingOverlay';
-import { AuthContext } from '../store/store'
-import AuthContent from "../components/AuthContent"
-import CocaColaTitle from "../UI/CokeHead"
-import HeadComp from '../components/HeadComp';
+import { AuthContext } from "../store/store";
+import AuthContent from "../components/AuthContent";
+import CocaColaTitle from "../UI/CokeHead";
+import Toast from "react-native-toast-message";
 
 const Login = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [error, setError] = useState(null)
-  const {authenticate, isAuthenticate} = useContext(AuthContext);
+  const { authenticate, isAuthenticate } = useContext(AuthContext);
 
-  console.log("login isauth", isAuthenticate)
+  const { data,mutate, isError, error, isPending } = useMutation({
+    mutationFn: LoginHander,
+    // the code below will wait the request to finish before moving to another page.
+    onMutate: async (data) => {
+      return data;
+    },
 
-  async function loginHandler({name, phone, region}){
-    try{
-      setIsAuthenticated(true)
-      const tokenData = { name, phone, region };
+    onSuccess: (data) => {
+      authenticate(data.name)
+    },
+  });
 
-      authenticate(JSON.stringify(tokenData))
-      setIsAuthenticated(false)
-
-
-    }catch(error){
-
-      if (error.response) {
-
-        // setError(error.response.data.error.message)
-        setError("Invalid credintials, Please check your details");
-
-        // console.log('Status Code:', error.response.status);
-        // console.log('Response Data:', error.response.data);
-        // console.log('Headers:', error.response.headers);
-
-
-      } else if (error.request) {
-        // The request was made, but no response was received
-        setError("Please check your network")
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error Message:', error.message);
-      }
-      setIsAuthenticated(false)
-    }
-    setIsAuthenticated(false)
+  function loginHandler({ name, password }) {
+    mutate({ name, password });
   }
 
   console.log(error);
 
-useEffect(() => {
-  if(error && !isAuthenticated){
-    Toast.show({
-      type: ALERT_TYPE.DANGER,
-      title: 'Error',
-      textBody: error,
-    })
-
-  }else if(error === "TOO_MANY_ATTEMPTS_TRY-LATER" && !isAuthenticated){
-    Toast.show({
-      type: ALERT_TYPE.DANGER,
-      title: 'Error',
-      textBody: 'Too many attempts try later',
-    })
-  }
-}, [error, isAuthenticated])
-
+  useEffect(() => {
+    if (error && !isPending) {
+      Toast.show({
+        type: "error",
+        text1: "Failed to log in",
+        text2: error.message,
+      });
+    } else if (error === "TOO_MANY_ATTEMPTS_TRY-LATER" && !isPending) {
+      Toast.show({
+        type: "error",
+        text1: "Error",
+        text2: "Too many attempts try later",
+      });
+    }
+  }, [error, isPending]);
 
   return (
     <View style={styles.screen}>
-         <View style={styles.cokeHeadStyle}>
-            <CocaColaTitle size={24}/>
-         </View>
-          <Text style={styles.text}>Please login in to continue.</Text>
-         <AuthContent isLogin isAuthenticate={isAuthenticate} onAuthenticate={loginHandler} isUpdating={false}/>
-
+      <View style={styles.cokeHeadStyle}>
+        <CocaColaTitle size={24} />
+      </View>
+      <Text style={styles.text}>Please login in to continue.</Text>
+      <AuthContent
+        isLogin
+        isAuthenticate={isAuthenticate}
+        onAuthenticate={loginHandler}
+        isUpdating={false}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
   screen: {
@@ -92,14 +74,13 @@ const styles = StyleSheet.create({
     marginTop: 48,
     marginHorizontal: 10,
     paddingVertical: 16,
-  //  justifyContent: '',
-    alignItems: 'flex-start'
+    //  justifyContent: '',
+    alignItems: "flex-start",
   },
   text: {
     marginHorizontal: 26,
     paddingHorizontal: 6,
     color: "gray",
     fontSize: 16,
-  }
-
-})
+  },
+});
--- screens/Profile.js
+++ screens/Profile.js
@@ -5,95 +5,107 @@
 // 4. A button to edit the profile
 // 5. A User name
 
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { View, Text, StyleSheet, Alert } from 'react-native'
-import React, { useContext, useEffect, useState } from 'react'
-import { GlobalStyles } from '../Constants/Globalcolors'
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, Text, StyleSheet, Alert } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { GlobalStyles } from "../Constants/Globalcolors";
 
-
-import { AuthContext } from '../store/store';
-import ProfilePic from '../components/ProfilePic'
-import UserName from '../components/UserName'
-import ProfileIconUI from '../UI/ProfileIcon'
-import { useNavigation, useIsFocused} from '@react-navigation/native';
+import { AuthContext } from "../store/store";
+import ProfilePic from "../components/ProfilePic";
+import UserName from "../components/UserName";
+import ProfileIconUI from "../UI/ProfileIcon";
+import { useNavigation, useIsFocused } from "@react-navigation/native";
 
 const Profile = () => {
-   const authctx =  useContext(AuthContext)
-    const [name, setName] = useState("");
-    const [phone, setPhone] = useState("");
-    const [region, setRegion] = useState("");
-    const navigation = useNavigation()
-    const isFocused = useIsFocused();
+  const authctx = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
 
-
-    // fetching user name from storage
-    useEffect(() => {
-      async function handleToken() {
-        const token = await AsyncStorage.getItem("token")
-         if(token){
-            const user = JSON.parse(token);
-            setName(user.name);
-            setPhone(user.phone);
-            setRegion(user.region);
-         }
+  // fetching user name from storage
+  useEffect(() => {
+    async function handleToken() {
+      const token = await AsyncStorage.getItem("token");
+      if (token) {
+        setName(token);
       }
+    }
 
-      handleToken();
-    }, [isFocused])
+    handleToken();
+  }, [isFocused]);
 
-     function editHandler (){
-        navigation.navigate("Edit Profile", {
-          name: name,
-          phone: phone,
-          region: region
-        })
-     }
+  function editHandler() {
+    navigation.navigate("Edit Profile", {
+      name: name,
+      phone: phone,
+      region: region,
+    });
+  }
 
-     function signoutHandler (){
-       Alert.alert("Sign out", "Are you sure you want to sign out?", [{text: 'Confirm', onPress: () => {
-        console.log("signing off");
-        authctx.logout();
-       }}, {text: 'Cancel', style: 'cancel'}])
-     }
+  function signoutHandler() {
+    Alert.alert("Sign out", "Are you sure you want to sign out?", [
+      {
+        text: "Confirm",
+        onPress: () => {
+          console.log("signing off");
+          authctx.logout();
+        },
+      },
+      { text: "Cancel", style: "cancel" },
+    ]);
+  }
 
   return (
     <View style={styles.screen}>
-       <View style={styles.screenChild}>
-           <ProfilePic/>
-           <UserName name={name} phone={phone}/>
-       </View>
-       {/* edit profile */}
-       <View style={styles.screen2}>
-          <ProfileIconUI name="person" size={24} color={GlobalStyles.colors.primary800} onPress={editHandler} text="Edit profile" bg="#fff"/>
+      <View style={styles.screenChild}>
+        <ProfilePic />
+        <UserName name={name} />
+      </View>
+      {/* edit profile */}
+      <View style={styles.screen2}>
+        <ProfileIconUI
+          name='person'
+          size={24}
+          color={GlobalStyles.colors.primary800}
+          onPress={editHandler}
+          text='Edit profile'
+          bg='#fff'
+        />
 
-          {/* signout */}
-          <ProfileIconUI name="log-out" size={24} color={GlobalStyles.colors.gray800} onPress={signoutHandler} text="Sign Out" bg="#fff"/>
-       </View>
+        {/* signout */}
+        <ProfileIconUI
+          name='log-out'
+          size={24}
+          color={GlobalStyles.colors.gray800}
+          onPress={signoutHandler}
+          text='Sign Out'
+          bg='#fff'
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-    },
-    screenChild: {
-      alignItems: 'center',
-      paddingVertical: 25,
-    },
-    screen2: {
-      padding: 10,
-      backgroundColor: GlobalStyles.colors.primary50,
-      height: "100%",
-      borderTopLeftRadius: 40,
-      borderTopRightRadius: 40,
-      elevation: 6,
-      shadowColor: 'black',
-      shadowOffset: {width: 0, height: 2},
-      shadowRadius: 6,
-      shadowOpacity: 0.26,
-
-    }
-})
+  screen: {
+    flex: 1,
+  },
+  screenChild: {
+    alignItems: "center",
+    paddingVertical: 25,
+  },
+  screen2: {
+    padding: 10,
+    backgroundColor: GlobalStyles.colors.primary50,
+    height: "100%",
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    elevation: 6,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+  },
+});
