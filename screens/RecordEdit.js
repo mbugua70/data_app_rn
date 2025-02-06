2e06927c908abd5dc3209251d4ecc8900e2d9c48
Code MBU <johnmbugua23code@gmail.com>
2025-02-06 17:07:11 +0300

    setting up the record edit screen

--- screens/RecordEdit.js
+++ screens/RecordEdit.js
@@ -1,23 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import AuthContentTwo from '../components/AuthContentTwo'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import AuthContentTwo from "../components/AuthContentTwo";
 
-const RecordEdit = ({route}) => {
-    const {formID, formTitle} = route.params;
-
-    console.log(formID, "record id")
-    console
+const RecordEdit = ({ route }) => {
+  const { formID, formTitle } = route.params;
   return (
     <View style={styles.screen}>
-       <AuthContentTwo formID={formID} formTitle={formTitle} />
+      <AuthContentTwo isEditing={true} formID={formID} formTitle={formTitle} />
     </View>
-  )
-}
+  );
+};
 
-export default RecordEdit
+export default RecordEdit;
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-    }
-})
+  screen: {
+    flex: 1,
+  },
+});
